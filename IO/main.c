6f088f77ff48a614b46c91fe4add72299966a76b
Pluto <wmpluto@gmail.com>
2018-04-05 10:52:09 +0900

    New Version

--- IO/main.c
+++ IO/main.c
@@ -13,11 +13,11 @@
 #define SFID 0x01 // MAN CODE: 0x01, NRZ CODE: 0xAAA9
 #define LENGTH 9
 
-unsigned char lookUp[16] = {0xAA, 0xA9, 0xA6, 0xA5, 0x9A, 0x99, 0x96, 0x95, 0x6A, 0x69, 0x66, 0x65, 0x5A, 0x59, 0x56, 0x55};
+uint8_t lookUp[16] = {0xAA, 0xA9, 0xA6, 0xA5, 0x9A, 0x99, 0x96, 0x95, 0x6A, 0x69, 0x66, 0x65, 0x5A, 0x59, 0x56, 0x55};
 
-char flag = 1;
+volatile char flag = 1;
 uint16_t p_uint16 = 0;
-uint8_t data[LENGTH] = {0x00, SFID, 0x14, 0x13, 0x12, 0x11, 0xCC, 0xCC, 0xCC};
+uint8_t data[LENGTH] = {PREMBLE, SFID, 0x14, 0x13, 0x12, 0x11, 0xCC, 0xCC, 0x01};
 uint16_t man_data[LENGTH];
 
 void Timer_Init()
@@ -28,7 +28,7 @@ void Timer_Init()
 	TCCR1B = 0;     // same for TCCR1B
 	
 	// set compare match register to desired timer count:
-	OCR1A = 125; //symbol length 125us
+	OCR1A = 125+1; //symbol length 125us
 	// turn on CTC mode:
 	TCCR1B |= (1 << WGM12);
 	// Set CS11 bits for 8 prescaler:
@@ -39,20 +39,23 @@ void Timer_Init()
 	sei();
 }
 
+void Timer_Close()
+{
+	cli();
+	TCCR1A = 0;     // set entire TCCR1A register to 0
+	TCCR1B = 0;     // same for TCCR1B
+}
+
 ISR(TIMER1_COMPA_vect)
 {
-	uint8_t i = p_uint16 >> 4;
-	static uint16_t converted_data = 0;
-	if ((p_uint16 & 0x000F) == 0x0000)
-		converted_data = (lookUp[data[i] >> 4]) | (lookUp[data[i] & 0x0F] << 8);
-    PORTA = (uint8_t)((converted_data >> (15 - (p_uint16++ & 0x000F))) & 0x0001);
-	
-	if ((p_uint16 >> 4) == 2) {
-		cli();
-		TCCR1A = 0;     // set entire TCCR1A register to 0
-		TCCR1B = 0;     // same for TCCR1B
+	if ((p_uint16 >> 4) == LENGTH) {
+		Timer_Close();
+		flag = 0;
+		PORTA = 0x00;
+		return;
 	}
-	//PORTA = ~PORTA;
+    PORTA = (uint8_t)((man_data[p_uint16 >> 4] >> (15 - (p_uint16 & 0x000F))) & 0x0001);
+	p_uint16++;
 }
 
 int main(void)
@@ -60,10 +63,14 @@ int main(void)
 	DDRA |= 1 << DDA0;
 	PORTA = 0x00;
 	
+	for (char ii = 0; ii < LENGTH; ii++) {
+		man_data[ii] = (lookUp[data[ii] >> 4]  << 8) | lookUp[data[ii] & 0x0F];
+	}
 
 	flag = 1;
 	Timer_Init();
-
-	while(1){};
+	while(flag){};
+	
+	while(1);
 }
 
