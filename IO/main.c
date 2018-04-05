c6fb380bf8807544071ac40b8d7ebd1a22566f6d
Pluto <wmpluto@gmail.com>
2018-04-05 09:50:29 +0900

    tmp

--- IO/main.c
+++ IO/main.c
@@ -7,33 +7,18 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 
-#define PRE_CNT 10*8;
-#define DATA_CNT 8
-#define DATA_MAN_BITS (2*DATA_CNT*8 + 1)
+#define PREMBLE 0x00 // MAN CODE: 0x00, NRZ CODE: 0xAAAA 
+#define SFID 0x01 // MAN CODE: 0x01, NRZ CODE: 0xAAA9
+#define LENGTH 9
 
 unsigned char lookUp[16] = {0xAA, 0xA9, 0xA6, 0xA5, 0x9A, 0x99, 0x96, 0x95, 0x6A, 0x69, 0x66, 0x65, 0x5A, 0x59, 0x56, 0x55};
 
-
-uint8_t cnt = 0, d_flag = 0;
-uint8_t pre[8] = {1,0,1,0,1,0,1,0};
-uint8_t sfid[8] = {1,0,1,0,1,0,0,1};
-uint8_t data[DATA_CNT] = {0x14, 0x13, 0x12, 0x11, 0xCC, 0xCC, 0xCC, 0xCC};
-uint8_t data_man[DATA_MAN_BITS];
-/* = {
-	//ID
-	1,0, 1,0, 1,0, 0,1, 1,0, 0,1, 1,0, 1,0,//0x14
-	1,0, 1,0, 1,0, 0,1, 1,0, 1,0, 0,1, 0,1,//0x13
-	1,0, 1,0, 1,0, 0,1, 1,0, 1,0, 0,1, 1,0,//0x12
-	1,0, 1,0, 1,0, 0,1, 1,0, 1,0, 1,0, 0,1,//0x11
-	//data
-	1,0, 1,0, 1,0, 0,1, 1,0, 0,1, 1,0, 1,0,//0x14
-	1,0, 1,0, 1,0, 0,1, 1,0, 1,0, 0,1, 0,1,//0x13
-	1,0, 1,0, 1,0, 0,1, 1,0, 1,0, 0,1, 1,0,//0x12
-	1,0, 1,0, 1,0, 0,1, 1,0, 1,0, 1,0, 0,1,//0x11
-	0
-};*/
+char flag = 1;
+uint16_t p_uint16 = 0;
+uint8_t data[LENGTH] = {0x00, SFID, 0x14, 0x13, 0x12, 0x11, 0xCC, 0xCC, 0xCC};
+uint16_t man_data[LENGTH];
 
 void Timer_Init()
 {
@@ -56,55 +41,29 @@ void Timer_Init()
 
 ISR(TIMER1_COMPA_vect)
 {
-	static uint8_t i = 0;
-	uint8_t *p = data_man;
+	uint8_t i = p_uint16 >> 4;
+	static uint16_t converted_data = 0;
+	if ((p_uint16 & 0x000F) == 0x0000)
+		converted_data = (lookUp[data[i] >> 4]) | (lookUp[data[i] & 0x0F] << 8);
+    PORTA = (uint8_t)((converted_data >> (15 - (p_uint16++ & 0x000F))) & 0x0001);
 	
-	if (cnt > 0) {
-		PORTA = pre[i];
-		cnt--;
-		i++;
-		if (i > 7) i = 0;
-	} else {
-		if (d_flag) {
-			PORTA = p[i];
-			i++;
-			if (i == DATA_MAN_BITS) {
-				TCCR1A = 0;     // set entire TCCR1A register to 0
-				TCCR1B = 0;     // same for TCCR1B
-			}
-		} else {
-			PORTA = sfid[i];
-			i++;
-			if (i > 7) {
-				i = 0;
-				d_flag = 1;
-			}
-		}
+	if ((p_uint16 >> 4) == 2) {
+		cli();
+		TCCR1A = 0;     // set entire TCCR1A register to 0
+		TCCR1B = 0;     // same for TCCR1B
 	}
+	//PORTA = ~PORTA;
 }
 
 int main(void)
 {
 	DDRA |= 1 << DDA0;
+	PORTA = 0x00;
 	
-	uint8_t *tmp = data_man;
-	for(uint16_t ii = 0; ii < DATA_CNT; ii++) {
-		uint8_t high = lookUp[(data[ii] >> 4)];
-		for(int8_t jj = 7; jj >= 0; jj--)
-			*tmp++ = (high >> jj) & 0x01;
-			
-		uint8_t low = lookUp[(data[ii] & 0x0F)];
-		for(int8_t jj = 7; jj >= 0; jj--)
-			*tmp++ = (low >> jj) & 0x01;
-	}
-	
-	cnt = PRE_CNT;
-	d_flag = 0;
+
+	flag = 1;
 	Timer_Init();
-	_delay_us(700);
-	
-    while (1) 
-    {
-    }
+
+	while(1){};
 }
 
