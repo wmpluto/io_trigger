03f3fcad83ebf0fb38e19b48f5128647b8d19c38
Pluto <wmpluto@gmail.com>
2018-04-05 17:53:00 +0900

    Fix bug

--- IO/main.c
+++ IO/main.c
@@ -17,8 +17,10 @@ unsigned char lookUp[16] = {0xAA, 0xA9, 0xA6, 0xA5, 0x9A, 0x99, 0x96, 0x95, 0x6A
 
 char flag = 1;
 uint16_t p_uint16 = 0;
-uint8_t data[LENGTH] = {0x00, SFID, 0x14, 0x13, 0x12, 0x11, 0xCC, 0xCC, 0xCC};
+uint8_t data[LENGTH] = {PREMBLE, SFID, 0x14, 0x13, 0x12, 0x11, 0xCC, 0xCC, 0x01};
 uint16_t man_data[LENGTH];
+uint8_t output = 0;
+uint16_t converted_data=0;
 
 void Timer_Init()
 {
@@ -28,7 +30,7 @@ void Timer_Init()
 	TCCR1B = 0;     // same for TCCR1B
 	
 	// set compare match register to desired timer count:
-	OCR1A = 125+1; //symbol length 125us
+	OCR1A = 125; //symbol length 125us
 	// turn on CTC mode:
 	TCCR1B |= (1 << WGM12);
 	// Set CS11 bits for 8 prescaler:
@@ -42,18 +44,17 @@ void Timer_Init()
 ISR(TIMER1_COMPA_vect)
 {
 	uint8_t i = p_uint16 >> 4;
-	if (i == 3) {
-		cli();
-		TCCR1A = 0;     // set entire TCCR1A register to 0
-		TCCR1B = 0;     // same for TCCR1B
+	if (i == LENGTH) {
 		PORTA = 0x00;
+		TCCR1B = 0;
+		cli();
 		return;
 	}
-	
-	static uint16_t converted_data = 0;
+	PORTA = output;
+	p_uint16++;
 	if ((p_uint16 & 0x000F) == 0x0000)
-		converted_data = (lookUp[data[i] >> 4] << 8 ) | lookUp[data[i] & 0x0F] ;
-    PORTA = (uint8_t)((converted_data >> (15 - (p_uint16++ & 0x000F))) & 0x0001);
+		converted_data = (lookUp[data[i+1] >> 4] << 8 ) | lookUp[data[i+1] & 0x0F] ;
+    output = (uint8_t)((converted_data >> (15 - (p_uint16 & 0x000F))) & 0x0001);
 }
 
 int main(void)
@@ -63,6 +64,8 @@ int main(void)
 	
 
 	flag = 1;
+	converted_data = (lookUp[data[0] >> 4] << 8 ) | lookUp[data[0] & 0x0F] ;
+	output = (uint8_t)((converted_data >> (15 - (p_uint16 & 0x000F))) & 0x0001);
 	Timer_Init();
 
 	while(1){};
