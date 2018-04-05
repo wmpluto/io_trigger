cca17a46c17b4d1f9cb2c89fc8e43fcc272cea9c
Pluto <wmpluto@gmail.com>
2018-04-05 15:42:12 +0900

    8bit

--- IO/main.c
+++ IO/main.c
@@ -16,9 +16,9 @@
 uint8_t lookUp[16] = {0xAA, 0xA9, 0xA6, 0xA5, 0x9A, 0x99, 0x96, 0x95, 0x6A, 0x69, 0x66, 0x65, 0x5A, 0x59, 0x56, 0x55};
 
 volatile char flag = 1;
-uint16_t p_uint16 = 0;
-uint8_t data[LENGTH] = {PREMBLE, SFID, 0x14, 0x13, 0x12, 0x11, 0xCC, 0xCC, 0x01};
-uint16_t man_data[LENGTH];
+volatile uint8_t p_uint8 = 0;
+uint8_t data[LENGTH] = {PREMBLE, PREMBLE, PREMBLE, PREMBLE, PREMBLE, SFID, 0xCC, 0xCC, 0x01};
+uint8_t man_data[LENGTH*2];
 
 void Timer_Init()
 {
@@ -28,7 +28,7 @@ void Timer_Init()
 	TCCR1B = 0;     // same for TCCR1B
 	
 	// set compare match register to desired timer count:
-	OCR1A = 125+1; //symbol length 125us
+	OCR1A = 125; //symbol length 125us
 	// turn on CTC mode:
 	TCCR1B |= (1 << WGM12);
 	// Set CS11 bits for 8 prescaler:
@@ -48,14 +48,18 @@ void Timer_Close()
 
 ISR(TIMER1_COMPA_vect)
 {
-	if ((p_uint16 >> 4) == LENGTH) {
+	uint8_t counter = p_uint8 >> 3;
+	if ((counter) == 5) {
 		Timer_Close();
 		flag = 0;
-		PORTA = 0x00;
+		PORTA = 0x01;
 		return;
 	}
-    PORTA = (uint8_t)((man_data[p_uint16 >> 4] >> (15 - (p_uint16 & 0x000F))) & 0x0001);
-	p_uint16++;
+
+
+	PORTA =  man_data[counter] >> ((8 + ~p_uint8) & 0x07);
+	//PORTA = ~PORTA;
+	p_uint8 = p_uint8 + 1;
 }
 
 int main(void)
@@ -63,13 +67,14 @@ int main(void)
 	DDRA |= 1 << DDA0;
 	PORTA = 0x00;
 	
-	for (char ii = 0; ii < LENGTH; ii++) {
-		man_data[ii] = (lookUp[data[ii] >> 4]  << 8) | lookUp[data[ii] & 0x0F];
+	for (char ii = 0, jj = 0; ii < LENGTH; ii++) {
+		man_data[jj++] = lookUp[data[ii] >> 4];
+		man_data[jj++] = lookUp[data[ii] & 0x0F];
 	}
 
 	flag = 1;
 	Timer_Init();
-	while(flag){};
+	//while(flag){};
 	
 	while(1);
 }
