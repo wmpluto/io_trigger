4921d25b6c2aae4ace795c99fd9e0cb46bb3d821
Pluto <wmpluto@gmail.com>
2018-04-05 17:18:42 +0900

    Fix bug
    进中断后先切IO口，复杂运算之后再做

--- IO/main.c
+++ IO/main.c
@@ -17,8 +17,9 @@ uint8_t lookUp[16] = {0xAA, 0xA9, 0xA6, 0xA5, 0x9A, 0x99, 0x96, 0x95, 0x6A, 0x69
 
 volatile char flag = 1;
 uint16_t p_uint16 = 0;
-uint8_t data[LENGTH] = {PREMBLE, SFID, 0x14, 0x13, 0x12, 0x11, 0xCC, 0xCC, 0x01};
+uint8_t data[LENGTH] = {PREMBLE, PREMBLE, PREMBLE, SFID, 0x14, 0x13, 0x12, 0x11, 0xCC};
 uint16_t man_data[LENGTH];
+uint8_t output = 0;
 
 void Timer_Init()
 {
@@ -26,6 +27,7 @@ void Timer_Init()
 	cli();          // disable global interrupts
 	TCCR1A = 0;     // set entire TCCR1A register to 0
 	TCCR1B = 0;     // same for TCCR1B
+	TCNT1 = 0;
 	
 	// set compare match register to desired timer count:
 	OCR1A = 125+1; //symbol length 125us
@@ -48,14 +50,15 @@ void Timer_Close()
 
 ISR(TIMER1_COMPA_vect)
 {
-	if ((p_uint16 >> 4) == LENGTH) {
+	if ((p_uint16++ >> 4) == LENGTH) {
 		Timer_Close();
 		flag = 0;
 		PORTA = 0x00;
 		return;
 	}
-    PORTA = (uint8_t)((man_data[p_uint16 >> 4] >> (15 - (p_uint16 & 0x000F))) & 0x0001);
-	p_uint16++;
+	
+    PORTA = output;
+	output = (uint8_t)((man_data[p_uint16 >> 4] >> ((16 + ~p_uint16) & 0x000F)) & 0x0001);
 }
 
 int main(void)
@@ -68,6 +71,7 @@ int main(void)
 	}
 
 	flag = 1;
+	output = (uint8_t)((man_data[p_uint16 >> 4] >> ((16 + ~p_uint16) & 0x000F)) & 0x0001);
 	Timer_Init();
 	while(flag){};
 	
